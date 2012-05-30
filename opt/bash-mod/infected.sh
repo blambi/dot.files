639d62fa9d8d7f45c334c28ad5bed305d72cc409
Patrik Lembke <patrik.lembke@nodeone.se>
2012-05-30 15:59:08 +0200

    For cupcake

--- opt/bash-mod/infected.sh
+++ opt/bash-mod/infected.sh
@@ -1,5 +1,5 @@
 function infected() {
-    pushd ~/progammering/dot.files/ > /dev/null
+    pushd /home/patrik/programmering/dot.files/ > /dev/null
     if [ "$( git st | wc -l)" -gt 2 ]; then
         echo "********* WARNING ********"
         echo "infection is quite posible"
